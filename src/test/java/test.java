7a471428ec121610add5f3444251d379184b4343
dstrzelbicki <93035670+dstrzelbicki@users.noreply.github.com>
2023-02-01 16:02:36 +0100

    add navigation

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,13 +1,38 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class test {
-    @Test
-    public void demoTest(){
+
+    WebDriver driver;
+
+    @BeforeEach
+    public void driverSetup(){
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.navigate().to("https://xtm.cloud/pl/formularz-zakladania-konta-probnego-xtm-cloud/");
+        driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1280, 720));
+    }
+
+    @AfterEach
+    public void closeAndQuit(){
+        driver.close();
+        driver.quit();
+    }
+
+    @Test
+    public void navigate(){
+        driver.get("https://allegro.pl");
+        driver.navigate().to("https://www.olx.pl");
+        driver.navigate().back();
+        String allegroTitle = "Allegro - atrakcyjne ceny - Strona Główna";
+        Assertions.assertEquals(allegroTitle, driver.getTitle(), "The title of the page is not: " + allegroTitle);
+        driver.navigate().forward();
+        String olxTitle = "Ogłoszenia - Sprzedam, kupię na OLX.pl";
+        Assertions.assertEquals(olxTitle, driver.getTitle(), "The title of the page is not: " + olxTitle);
     }
 
 }
